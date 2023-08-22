898239ce3dd496297e22339c93961ecd894792fc
Kevin Frost <biztos@mac.com>
2023-08-22 10:22:38 +0700

    improved test db handling
    new db for every test run

--- yaqpg_suite_test.go
+++ yaqpg_suite_test.go
@@ -5,12 +5,15 @@ package yaqpg_test
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/biztos/yaqpg"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/oklog/ulid/v2"
 	"github.com/stretchr/testify/suite"
 )
@@ -31,8 +34,11 @@ type DoesNotMarshal string
 
 type YaqpgTestSuite struct {
 	suite.Suite
-	Queue  *yaqpg.Queue
-	Logger *TestLogger
+	Queue               *yaqpg.Queue
+	Logger              *TestLogger
+	Database            string
+	OriginalDatabaseUrl string
+	KeepDatabase        bool
 }
 
 func (suite *YaqpgTestSuite) AssertLogged(msgs ...string) {
@@ -52,7 +58,45 @@ func (suite *YaqpgTestSuite) SetupSuite() {
 
 	require := suite.Require()
 
-	yaqpg.DefaultTableName = fmt.Sprintf("yaqpg_test_%s", ulid.Make())
+	// It's a pain running tests against even a dev database, because we have
+	// to zero out the data all the time, and it's not realistic to have the
+	// developer always remember to set the correct database, so we override
+	// that here and create a new test database.
+	//
+	// Doing this via DATABASE_URL is messy but seems like the only way that
+	// will (should) work.
+	dbname := strings.ToLower(fmt.Sprintf("yaqpg_test_%s", ulid.Make()))
+	orig_dburl := os.Getenv("DATABASE_URL")
+	if orig_dburl == "" {
+		// If you forgot to set it, we assume (dangerously?) the default.
+		orig_dburl = "postgresql://localhost:5432/"
+	}
+	u, err := url.Parse(orig_dburl)
+	require.NoError(err, "parse env DATABASE_URL")
+	u.Path = "/" + dbname
+	test_dburl := u.String()
+
+	// Now we can create that database, with a connection from the existing
+	// one.  For this we don't use a pool because we just want the one exec.
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, orig_dburl)
+	require.NoError(err, "connect to database for create")
+	sql := fmt.Sprintf("CREATE DATABASE %s TEMPLATE template0;", dbname)
+	_, err = conn.Exec(ctx, sql)
+	conn.Close(ctx)
+	require.NoError(err, "exec create database")
+
+	// All further operations are on this new, test database.
+	suite.Database = dbname
+	suite.OriginalDatabaseUrl = orig_dburl
+	os.Setenv("DATABASE_URL", test_dburl)
+
+	// And we may wish to keep it...
+	keep := os.Getenv("KEEP_DB")
+	if keep != "" && keep != "0" && keep != "false" {
+		suite.KeepDatabase = true
+	}
+
 	yaqpg.Now = func() time.Time {
 		return time.Time{}
 	}
@@ -69,17 +113,23 @@ func (suite *YaqpgTestSuite) TearDownSuite() {
 
 	require := suite.Require()
 
-	val := os.Getenv("KEEP_DB")
-	if val != "" && val != "0" && val != "false" {
+	// We can't drop the database if it has an open pool.
+	suite.Queue.Pool.Close()
+
+	if suite.KeepDatabase {
+		fmt.Println("Keeping database:", suite.Database)
 		return
 	}
 
-	require.NotNil(suite.Queue, "queue")
-	require.NotNil(suite.Queue.Pool, "pool")
-
-	sql := fmt.Sprintf("DROP TABLE IF EXISTS %s;", suite.Queue.TableName)
-	_, err := suite.Queue.Pool.Exec(context.Background(), sql)
-	require.NoError(err, "drop exec")
+	// In order to drop the test database we need a connection to the original,
+	// which we have fortunately kept!
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, suite.OriginalDatabaseUrl)
+	require.NoError(err, "connect to original database")
+	sql := fmt.Sprintf("DROP DATABASE %s;", suite.Database)
+	_, err = conn.Exec(ctx, sql)
+	defer conn.Close(ctx)
+	require.NoError(err, "exec drop database")
 
 }
 
